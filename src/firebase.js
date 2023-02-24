02aa1e60c2b2194e06218ff1d68aae9efeaa542f
with_blue <yhdosa0622@gmail.com>
2023-02-24 17:30:23 +0900

    Router 세팅

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAesOGn4Cb2eSMzNNbogjpBkdef93bp2E",
-  authDomain: "nwitter-ed434.firebaseapp.com",
-  projectId: "nwitter-ed434",
-  storageBucket: "nwitter-ed434.appspot.com",
-  messagingSenderId: "1068207244607",
-  appId: "1:1068207244607:web:8117061d543b978cc2ff8a",
-  measurementId: "G-F66LJF8PER"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
