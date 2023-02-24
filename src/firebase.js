8b73fe28ef02494da2b926be872bcc353a4f89b3
with_blue <yhdosa0622@gmail.com>
2023-02-24 16:40:42 +0900

    firebase 세팅

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAesOGn4Cb2eSMzNNbogjpBkdef93bp2E",
+  authDomain: "nwitter-ed434.firebaseapp.com",
+  projectId: "nwitter-ed434",
+  storageBucket: "nwitter-ed434.appspot.com",
+  messagingSenderId: "1068207244607",
+  appId: "1:1068207244607:web:8117061d543b978cc2ff8a",
+  measurementId: "G-F66LJF8PER"
+};
+
+export const app = initializeApp(firebaseConfig);
